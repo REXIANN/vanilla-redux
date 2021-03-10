7c58da63eefd98ab41a1435ab8eb07d3b1ccded0
김진혁 <gandolkim@gmail.com>
2021-03-10 20:47:29 +0900

    basic redux done

--- src/index.js
+++ src/index.js
@@ -6,10 +6,31 @@ const number = document.querySelector("span")
 
 
 
-const countModifier = (state = 0) => {
-  return state
+const countModifier = (count = 0, action) => {
+  // console.log(action)
+  if (action.type ==="ADD") {
+    return count + 1
+  } else if (action.type === 'MINUS') {
+    return count - 1
+  }
+  return count
 }
  
 const countStore = createStore(countModifier)
 
-console.log(countStore, countStore.getState())
+const onChange = () => {
+  // console.log(countStore.getState())
+  number.innerText = countStore.getState()
+}
+
+countStore.subscribe(onChange)
+
+// console.log(countStore, countStore.getState())
+
+countStore.dispatch({ type: "ADD" })
+
+// console.log(countStore.getState())
+
+add.addEventListener("click", () => countStore.dispatch({ type: 'ADD' }))
+minus.addEventListener("click", () => countStore.dispatch({ type: 'MINUS' }))
+
