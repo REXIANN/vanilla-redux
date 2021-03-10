9f07d373b75671d21c83d74ca369619c3e7952bf
김진혁 <gandolkim@gmail.com>
2021-03-10 22:20:59 +0900

    react-redux setup done

--- src/index.js
+++ src/index.js
@@ -1,39 +1,6 @@
-import { createStore } from 'redux'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
 
-const add = document.getElementById("add")
-const minus = document.getElementById("minus")
-const number = document.querySelector("span")
 
-const ADD = 'ADD'
-const MINUS = 'MINUS'
-
-const countModifier = (count = 0, action) => {
-  // console.log(action)
-  switch (action.type) {
-    case ADD:
-      return count + 1
-    case MINUS:
-      return count - 1
-    default:
-      return count
-  }
-}
- 
-const countStore = createStore(countModifier)
-
-const onChange = () => {
-  // console.log(countStore.getState())
-  number.innerText = countStore.getState()
-}
-
-countStore.subscribe(onChange)
-
-// console.log(countStore, countStore.getState())
-
-// countStore.dispatch({ type: "ADD" })
-
-// console.log(countStore.getState())
-
-add.addEventListener("click", () => countStore.dispatch({ type: ADD }))
-minus.addEventListener("click", () => countStore.dispatch({ type: MINUS }))
-
+ReactDOM.render(<App/>, document.getElementById('root'))
